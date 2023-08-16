862443ad76e1a63b6aec0010ef2180c38ca798e3
xiaoiver <pyqiverson@gmail.com>
2023-08-16 14:40:40 +0800

    fix: export Transformation3D (#45)
    * fix: export Transformation3D
    
    * chore: bump version to 0.4.6

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export type { Options, Vector, Transformation, Matrix3, Vector2, Vector3 } from './type';
+export type { Options, Vector, Transformation, Transformation3D, Matrix3, Vector2, Vector3 } from './type';
 export { Coordinate } from './coordinate';
 export { Coordinate3D } from './coordinate3D';
